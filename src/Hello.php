d6716f4b2bec0e9583a797c2eed8b6143dbf8ff3
manel <manelgavalda@iesebre.com>
2016-10-04 16:18:52 +0200

    first commit

---
+++ src/Hello.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manel
+ * Date: 4/10/16
+ * Time: 16:02
+ */
+
+namespace Manel;
+
+
+class Hello
+{
+    public static function Hola()
+    {
+        return 'Hello world!';
+    }
+
+}
---
+++ test/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manel
+ * Date: 4/10/16
+ * Time: 16:08
+ */
